aaab7b7f690af2b91749c8686a7feea5814cbda8
Adrian Zabrocki <a.zabrocki@kainos.com>
2016-07-12 09:55:41 +0200

    Move microservices into separate .tf files; Remove old comment
    Remove an outdated comment

--- Infrastructure/providers.tf
+++ Infrastructure/providers.tf
@@ -1,36 +1,3 @@
 provider "aws" {
 	region = "${var.region}"
 }
-
-resource "aws_instance" "msvc_1" {
-	ami = "${lookup(var.amis, var.region)}"
-	instance_type = "t2.micro"
-	key_name = "microservices"
-	security_groups = ["allow_all"]
-
-	tags {
-		Name = "msvc_1_instance"
-		Group = "api_gate_demo"
-	}
-}
-
-resource "aws_instance" "msvc_2" {
-	ami = "${lookup(var.amis, var.region)}"
-	instance_type = "t2.micro"
-	key_name = "microservices"
-	security_groups = ["allow_all"]
-
-	tags {
-		Name = "msvc_2_instance"
-		Group = "api_gate_demo"
-	}
-}
-
-resource "aws_eip" "ip_msvc_1" {
-	instance = "${aws_instance.msvc_1.id}"
-}
-
-resource "aws_eip" "ip_msvc_2" {
-	instance = "${aws_instance.msvc_2.id}"
-}
-
